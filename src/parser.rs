131e74536af1fea33d6d4a90f9fe50a8f10569c9
alisonrasnic <alisonrasnic@gmail.com>
2025-01-04 04:28:08 -0800

    Refactoring parser a bit

--- src/parser.rs
+++ src/parser.rs
@@ -1,77 +1,33 @@
 #[derive(Debug, PartialEq, Clone)]
 pub struct Parser<'a>{
-    /*
-     * so we want to do what
-     * we want to take in each token and determine what it is
-     * is it id? is it value? is it operator? is it keyword? is it delimiter?
-     * 
-     * but then what? 
-     *
-     * LR(1) parsing step
-     */
-
-    pub lex_tokens:  Vec<LexerToken<'a>>,
     pub parse_stack: Vec<ParserToken<'a>>
 }
 
 impl<'a> Parser<'_> {
     pub fn new<'b>() -> Parser<'b> {
-        Parser { lex_tokens: vec![], parse_stack: vec![] }
+        Parser { parse_stack: vec![] }
     }
 
-    pub fn parse_lexes(&mut self) {
-        for x in &self.lex_tokens {
-            self.parse_stack.push(ParserToken {parse_type: ParserTokenType::Identifier, literal: "x" } );
+    // lex is necessary to convert from string into a token
+    //  as opposed to the parse which changes tokens into simpler tokens
+    pub fn lex(&self, tokens: &Vec<String>) -> Vec<ParserToken> {
+        let mut lexes: Vec<ParserToken> = vec![];
+        
+        for s in tokens {
+            lexes.push(self::str_to_lex(s));
         }
-        //  
+    }
+
+    pub fn parse(&mut self, tokens: &Vec<ParserToken>) -> bool {
+        
     }
 
     pub fn shift() {
     }
 
     pub fn reduce() -> Result<&'static str, &'static str> {
-        // we take a token and its corresponding parser token type
-        // and then we see if it can be specified
-        // e.g "i32" value -> type
-        // e.g "mn" value -> identifier
-        // e.g "500" value -> number
-        //
-        // once it stops reducing, we shift
         Ok("Reduced TODO")
     }
-
-    pub fn lex_tokens(&mut self, tokens: &Vec<String>) {
-        for i in 0..tokens.len() {
-            let cur_token: &'static str = tokens[i].clone().leak();
-                                   
-            self.lex_tokens.push(LexerToken { lexer_type: Self::str_to_lex(&cur_token), literal: &cur_token} );
-        }
-    }
-
-    pub fn str_to_lex(str: &str) -> LexerTokenType {
-        match str {
-            "i32" => LexerTokenType::Keyword,
-            "u32" => LexerTokenType::Keyword,
-            "bool" => LexerTokenType::Keyword,
-            "u1"=>LexerTokenType::Keyword,
-            "str"=>LexerTokenType::Keyword,
-            "chr"=>LexerTokenType::Keyword,
-            "u8"=>LexerTokenType::Keyword,
-            "let"=>LexerTokenType::Keyword,
-            "mut"=>LexerTokenType::Keyword,
-            "+"=>LexerTokenType::Op,
-            "-"=>LexerTokenType::Op,
-            "*"=>LexerTokenType::Op,
-            "/"=>LexerTokenType::Op,
-            "^"=>LexerTokenType::Op,
-            "let"=>LexerTokenType::Op,
-            "{"=>LexerTokenType::Delimiter,
-            "}"=>LexerTokenType::Delimiter,
-            ";"=>LexerTokenType::Delimiter,
-            _=>LexerTokenType::Value,
-        }
-    }
-    
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -80,31 +36,92 @@ pub struct ParserToken<'a> {
     literal: &'a str,
 }
 
+// what kinds of things do we really need in parser tokens?
+// value op value reduces to value
+//
+//   ex:  
+//          i32 mn $args {
+//              put "Hello, World!";
+//              let x = 5 + 5;
+//              0
+//          }
+//
+//  this whole thing needs to reduce to an acceptable token
+//
+//  this is why the C expr token exists
+//
+//  so the whole thing needs to eval to expr
+//
+//  lets reduce the whole example so far
+//
+//  Type Reserved Arr Type Reserved Delimiter Id String Id Id Op Value Op Value Value Delimiter
+//
+//  Function Reserved Delimiter Function Expr Value Delimiter
+//  Function Reserved Expr
+//  Function
+//
+//  I think Func can also work as a final value, we just call the main function if it exists
+//
+//  * = 0+
+//  Grammar Rules:
+//      Type Id (Type Id Comma)* => Func
+//
+//      Id Value => Func
+//
+//      Id Id Op Value => Expr
+//
+//      Value Op Value => Expr
+//
+//      Func Id Expr Value => Func
+//
+//      String => Value
+//
+//  Let's interpret the ex using these rules and LR(1) parsing
+//
+//      Shift
+//
+//      "i32" "mn"
+//      type   mn
+//      type   id  $args
+//      type   id  type id
+//      
+//      func delimiter
+//      func delimiter put
+//      func delimiter id "Hello, world!"
+//      func delimiter id value
+//      func delimiter func
+//      func delimiter func let
+//      func delimiter func id  x
+//      func delimiter func id  id
+//      func delimiter func id  id =
+//      func delimiter func id  id op
+//      func delimiter func id  id op 5
+//      func delimiter func id  id op value
+//      func delimiter func expr
+//      func delimiter func func 0
+//      func delimiter func func value
+//      func delimiter func func value delimiter
+//      func delimiter func func value delimiter
+//
+//      we halt there on current rules
+//
+//      we add one more rule:
+//          func delimiter func* value delimiter => func
+//
+//      now we get our last reduce:
+//
+//      func
+//
 #[derive(Debug, PartialEq, Clone)]
 pub enum ParserTokenType {
     Value,
-    Identifier,
+    Expr,
+    Func,
+    Id,
     Op,
-    Keyword,
     Type,
-    Int32,
-    UInt32,
-    Boolean,
     Arr,
     Reserved,
-    Delimiter
-}
-
-#[derive(Debug, PartialEq, Clone)]
-pub struct LexerToken<'a> {
-    lexer_type: LexerTokenType,
-    literal: &'a str,
-}
-
-#[derive(Debug, PartialEq, Clone)]
-pub enum LexerTokenType {
-    Value,
-    Op,
-    Keyword,
     Delimiter,
+    Comma
 }
